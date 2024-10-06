3f33c427f84cd7b04a0ea81031fc4d72961de28a
6-keem <6ukeem@gmail.com>
2024-10-06 23:23:54 +0900

    🛠️ fix : Error handling when paging to the next page

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
     purrio_scraper = PurrioScraper()
     jsonl_file_handler = JsonlFileHandler('./')
 
-    #TODO while문으로 페이지 넘기는 로직 추가
-    faq_dict_list : list[DataClass] = purrio_scraper.get_faq()
-
-    #TODO 반환 받은 list[DataClass] jsonl 형식으로 저장
-    # jsonl_file_handler.
-    purrio_scraper.quit()
+    try : 
+        faq_dict_list : list[DataClass] = purrio_scraper.get_faq()
+        
+        #TODO 반환 받은 list[DataClass] jsonl 형식으로 저장
+        # jsonl_file_handler.
+    except Exception as e :
+        print(f'❌ 실패 {e}')
+    finally:
+        purrio_scraper.quit()
