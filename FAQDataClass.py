c06fb0fb944b87439da37cc60fe1b7f3ed4577ff
junni01kim <audwnssp@naver.com>
2024-10-07 11:07:33 +0900

    complete

--- FAQDataClass.py
+++ FAQDataClass.py
@@ -1,48 +1,48 @@
-class DataClass():
-    def __init__(self) -> None:
-        self.__title : str
-        self.__type : str
-        self.__answer : str 
-        self.__link : str
-    
-    #getter
-    @property 
+class DataClass:
+    def __init__(self, title: str = "", type: str = "", answer: str = "", link: str = "") -> None:
+        self.__title: str = title
+        self.__type: str = type
+        self.__answer: str = answer
+        self.__link: str = link
+
+    # getter
+    @property
     def title(self) -> str:
         return self.__title
-    
-    #setter
-    @title.setter 
-    def title(self, title):
+
+    # setter
+    @title.setter
+    def title(self, title: str):
         self.__title = title
 
-    #getter
-    @property 
+    # getter
+    @property
     def type(self) -> str:
         return self.__type
-    
-    #setter
-    @type.setter 
-    def type(self, type):
+
+    # setter
+    @type.setter
+    def type(self, type: str):
         self.__type = type
-    
-    #getter
-    @property   
+
+    # getter
+    @property
     def answer(self) -> str:
         return self.__answer
-    
-    #setter
-    @answer.setter 
-    def answer(self, answer):
+
+    # setter
+    @answer.setter
+    def answer(self, answer: str):
         self.__answer = answer
-    
-    #getter
-    @property 
+
+    # getter
+    @property
     def link(self) -> str:
         return self.__link
-    
-    #setter
-    @link.setter 
-    def link(self, link):
+
+    # setter
+    @link.setter
+    def link(self, link: str):
         self.__link = link
 
     # method to return the object as a dictionary
